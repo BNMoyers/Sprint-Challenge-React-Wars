904ad78ef941f5952ee3301c5a6d89e2487f3569
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-19 12:18:54 -0500

    data properly synced, components connected

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,26 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import GridBox from './components/Grid.js';
-import axios from 'axios';
 
 const App = () => {
-  const[character, setCharacter] = useState([]);
-  useEffect(() => {
-    axios
-      .get('https://swapi.co/api/people/')
-      .then(res => {
-        console.log(res.data.results);
-        setCharacter(res.data.results);
-      })
-      .catch(err => console.log(err));
-
-  },[])
+  
   
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
       <div>
-        <GridBox props={character} />
+        <GridBox />
       </div>
 
     </div>
--- starwars/src/components/Grid.js
+++ starwars/src/components/Grid.js
@@ -1,12 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import { Grid, Image } from 'semantic-ui-react'
 import Cards from './Cards';
+import axios from 'axios';
 
 
-const GridBox = (props) => (
+ function GridBox() {
+    const[character, setCharacter] = useState([]);
+    useEffect(() => {
+        axios
+        .get('https://swapi.co/api/people/')
+        .then(res => {
+          console.log('fetched data',res.data.results);
+          setCharacter(res.data.results);
+          console.log('character',character)
+        })
+  
+    },[])
+   
+     return(
   <Grid columns={3} divided>
-    <Cards />
+    {console.log('props check',character)}
+    {character.map(results => (
+        <Cards  
+            
+            name={results.name}
+            homeworld={results.homeworld}
+            birthYear={results.birthYear}
+            gender={results.gender}
+            films={results.films}/>
+    ))}  
   </Grid>
-)
-
+  );
+    }
+  
 export default GridBox
+
