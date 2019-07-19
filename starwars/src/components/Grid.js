6f8be90adc56c2d367d9fd48606e020895de3449
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-19 10:43:55 -0500

    set up app framework using Semantic-UI components

---
+++ starwars/src/components/Grid.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { Grid, Image } from 'semantic-ui-react'
+import Cards from './Cards';
+
+const GridBox = () => (
+  <Grid columns={3} divided>
+    <Grid.Row>
+      <Grid.Column>
+    
+      </Grid.Column>
+      <Grid.Column>
+        
+      </Grid.Column>
+      <Grid.Column>
+       
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row>
+      <Grid.Column>
+       
+      </Grid.Column>
+      <Grid.Column>
+       
+      </Grid.Column>
+      <Grid.Column>
+        
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
+
+export default GridBox
